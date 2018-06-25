8fceeb09c258393c79e3e6c0443d96ddbcc77e66
ggapp <ggapp@localhost.localdomain>
2018-06-25 00:08:18 -0300

    NN, SVM, RF e NB. Arquivo de output" temp"

--- main.py
+++ main.py
@@ -4,8 +4,62 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import svm
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 from keras import optimizers
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+
+def NN(x_train, x_test, y_train, y_test):
+	num_neurons = 1600
+	optimizer = optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0)
+	epochs = 10
+	batch_size = 20
+
+	model = Sequential() 
+	model.add(Dense(8, input_shape=(8,), activation='sigmoid'))
+	model.add(Dense(880, activation='sigmoid'))
+	model.add(Dense(540, activation='sigmoid'))
+	model.add(Dense(180, activation='sigmoid'))
+	model.add(Dense(1, activation='sigmoid'))
+	model.summary()
+	model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+	model.fit(x_train, y_train, epochs = epochs, batch_size = batch_size, verbose = 0)
+	nn_pred = model.predict_classes(x_test)
+	print "\n#### NN\n"
+	print accuracy_score(y_test, nn_pred)
+	print confusion_matrix(y_test, nn_pred)
+
+
+
+def SVM(x_train, x_test, y_train, y_test):
+	svm_classifier = svm.SVC(kernel='rbf',C=200, degree=5 ,gamma=0.0001)
+	svm_classifier.fit(x_train, y_train)
+
+	svm_pred = svm_classifier.predict(x_test)
+	print "\n#### SVM\n"
+	print accuracy_score(y_test, svm_pred)
+	print confusion_matrix(y_test, svm_pred)
+
+def randomForest(x_train, x_test, y_train, y_test):
+	rfc = RandomForestClassifier()
+	rfc.fit(x_train, y_train)
+	rfc_pred  = rfc.predict(x_test)
+	print "\n#### RF\n"
+	print accuracy_score(y_test, rfc_pred)
+	print confusion_matrix(y_test, rfc_pred)
+
+def naiveBayes(x_train, x_test, y_train, y_test):
+	nb = GaussianNB()
+	nb_pred = nb.fit(x_train, y_train).predict(x_test)
+	print "\n#### NB\n"
+	print accuracy_score(y_test, nb_pred)
+	print confusion_matrix(y_test, nb_pred)
+
+
+
+
 
 dataset = pd.read_csv('dataset/pulsar_stars.csv')
 #drop missing values
@@ -55,25 +109,13 @@ print 'true, false, total on test, % '
 print n_true, n_false, y_test.size, (n_true*100)/y_test.size
 
 #SVM
-
-svm_classifier = svm.SVC(gamma=0.001, C=100)
-svm_classifier.fit(x_train, y_train)
-
-svm_pred = svm_classifier.predict(x_test)
-
-print accuracy_score(y_test, svm_pred)
+SVM(x_train, x_test, y_train, y_test)
 
 #NN
+NN(x_train, x_test, y_train, y_test)
 
-num_neurons = 100
-optimizer = optimizers.SGD(lr=1)
-epochs = 50
-batch_size = 10
+## Random Forest
+randomForest(x_train, x_test, y_train, y_test)
 
-model = Sequential() 
-model.add(Dense(8, input_shape=(8,), activation='sigmoid'))
-model.add(Dense(num_neurons, activation='sigmoid'))
-model.add(Dense(1, activation='sigmoid'))
-model.summary()
-model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-model.fit(X_data, Y_data, epochs = epochs, batch_size = batch_size, verbose = 1)
+##Naive bayes
+naiveBayes(x_train, x_test, y_train, y_test)
